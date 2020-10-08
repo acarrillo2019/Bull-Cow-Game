0802008e9de49f61be036f6d81c09d1782bb2f59
acarrillo2019 <alric21@aol.com>
2020-10-08 10:29:09 -0700

    FStrings, Booleans, Pre and Post decrement

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -5,33 +5,46 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
 
-    // Welcome the Player
-    PrintLine(TEXT("Welcome Player!"));
-    PrintLine(TEXT("Guess the four letter word!")); // Magic number Remove
-    PrintLine(TEXT("Type something and then press enter!"));
+    SetupGame();
 
-    // Setting up game
-    HiddenWord = TEXT("rambo"); // Set the HiddenWord 
-    // Set Lives
-
-    // Prompt player for guess
+    PrintLine(TEXT("The Hidden Word is: %s. "), *HiddenWord); // Debug Line
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-    ClearScreen();
-
-    // Check PlayerGuess
-
-    if (Input==HiddenWord)
+    if (bGameOver)
     {
-        PrintLine(TEXT("You guessed correctly!"));
+        ClearScreen();
+        SetupGame();
     }
-    else
+    else //Checking PlayerGuess
     {
-        PrintLine(TEXT("You guessed wrong!"));
-    }
+        if (Input==HiddenWord)
+        {
+            PrintLine(TEXT("You guessed correctly!"));
+            EndGame();
+        }
+        else
+        {
+            PrintLine(TEXT("Lost a life!"));
+            PrintLine(TEXT("%i"), --Lives);
 
+            if (Lives > 0)
+            {
+                if (Input.Len() != HiddenWord.Len())
+                {
+                    PrintLine(TEXT("Sorry, try guessing again! \nYou have %i lives remaining."), Lives);
+                }
+                
+            }
+            else
+            {
+                PrintLine(TEXT("You have no lives left!"));
+                EndGame();
+            }
+        }
+    }
+    
     // Check if isogram
     // Prompt to guess again
     // Check right number of characters
@@ -46,7 +59,24 @@ void UBullCowCartridge::OnInput(const FString& Input) // When the player hits en
     // Prompt to Play Again, Press enter to play again
     // Check user input
     // PLay again or quit
+}
 
+void UBullCowCartridge::SetupGame()
+{
+    // Welcome the Player
+    PrintLine(TEXT("Welcome to Bull Cow!"));
 
-    
+    HiddenWord = TEXT("rambo");
+    Lives = HiddenWord.Len();
+    bGameOver = false;
+
+    PrintLine(TEXT("Guess the %i letter word!"), HiddenWord.Len());
+    PrintLine(TEXT("You have %i lives"), Lives);
+    PrintLine(TEXT("Type in your guess and \nPress enter to continue...")); // Prompt player for guess
+}
+
+void UBullCowCartridge::EndGame()
+{
+    bGameOver = true;
+    PrintLine(TEXT("Press enter to play again."));
 }
